46d4ca3d49d7d59bb758b6d1c9dcce05f978f9e0
patel4585 <dpl190@uregina.ca>
2025-11-29 14:16:02 -0600

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            label1 = new Label();
+            lstNames = new ListBox();
+            txtName = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(560, 141);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(125, 33);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add Name";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(99, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Names";
+            label1.Click += label1_Click;
+            // 
+            // lstNames
+            // 
+            lstNames.FormattingEnabled = true;
+            lstNames.Location = new Point(99, 108);
+            lstNames.Name = "lstNames";
+            lstNames.Size = new Size(430, 284);
+            lstNames.TabIndex = 2;
+            lstNames.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(560, 108);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(125, 27);
+            txtName.TabIndex = 3;
+            txtName.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtName);
+            Controls.Add(lstNames);
+            Controls.Add(label1);
+            Controls.Add(btnAdd);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load_1;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private Label label1;
+        private ListBox lstNames;
+        private TextBox txtName;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+using System.Xml.Linq;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
+        }
+    }
+}
